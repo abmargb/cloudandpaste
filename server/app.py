fd240656b62648c633971664ea16c2a63de90a96
Abmar Barros <abmargb@gmail.com>
2014-12-04 01:31:10 +0000

    making a dict out of clipboard

--- server/app.py
+++ server/app.py
@@ -3,19 +3,20 @@ from flask import Flask, request, abort
 
 app = Flask(__name__)
 
-clipboard = 'contents'
+clipboard = {}
+clipboard['contents'] = ''
 
 @app.route('/clipboard', methods=['GET'])
 def get_clipboard():
-    return clipboard
+    return clipboard['contents']
 
 @app.route('/clipboard', methods=['POST'])
 def update_clipboard():
     if not request.json or not 'contents' in request.json:
         abort(400)
     global clipboard
-    clipboard = request.json['contents']
-    return clipboard, 201
+    clipboard['contents'] = request.json['contents']
+    return clipboard['contents'], 201
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000, debug=True)
